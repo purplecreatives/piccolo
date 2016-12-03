979c1f4b989898431604b403ee1d527ef9e4e508
Anos Mhazo <mhazo@zedc.co.zw>
2016-12-03 21:11:26 +0200

    Init folder structure, and libraries

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # piccolo
 HTML5 image uploader and manipulation library for web projects
+
+Depends on
+
+1. jQuery 1.11.1 (
+2. MDL 1.2.1 (https://getmdl.io/started/index.html#download)
---
+++ src/dropfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Zesa on 12/3/2016.
+ */
+(function ($, me){
+
+    /**
+     * Do preliminary setup here of dom and of script
+     * Create dropzone
+     */
+
+}(jQuery, {}));
