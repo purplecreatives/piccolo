fd6e0b4d9f49175ae142447f988dd7c49dc9228f
Anos Mhazo <mhazo@zedc.co.zw>
2016-12-06 15:04:51 +0200

    Create image zone

--- src/imagezone.js
+++ src/imagezone.js
@@ -9,9 +9,35 @@
     me.createImagezone = function(parent){
 
         var $parent = $(parent);
+        $parent.addClass('piccolo');
 
         //Remove all events on parent
         $parent.off();
+        $parent.empty();
+
+        //Card DIV
+        var $card = $('<canvas/>', {
+            "class": "mdl-card mdl-shadow--2dp"
+        });
+
+        //Card Menu
+        var $card_menu = $('<div/>',{
+            "class": "mdl-card__menu"
+        });
+
+        $card_menu.append($('<button/>', {
+            "class": "mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"
+        }).append($('<i/>',{
+            "class": "material-icons"
+        }).html('crop')));
+
+        //Add card content, summary text, and actions
+        //to material card
+        $card.append($card_menu);
+
+        //Add material card to parent
+        //container
+        $parent.append($card);
 
     }
 
