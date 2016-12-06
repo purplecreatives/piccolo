de6925eba266100fdec2470006e5fb18a77f11a6
Anos Mhazo <mhazo@zedc.co.zw>
2016-12-06 12:22:36 +0200

    Add imagezone js and create imagezone function

---
+++ src/imagezone.js
@@ -0,0 +1,17 @@
+/**
+ * Created by AK Mhazo on 12/6/2016.
+ */
+(function($, me){
+
+    me.createImagezone = function(parent){
+
+        var $parent = $(parent);
+
+        //Remove all events on parent
+        $parent.off();
+
+    }
+
+
+
+}(jQuery, Piccolo));
