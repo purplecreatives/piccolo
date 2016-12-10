1fafb357586eb9998ecdc687601f4cd0493adcb1
Shady3cho <langtonmwanza@gmail.com>
2016-12-10 19:50:49 +0200

    Adding of rotation and cropping

--- src/edit.js
+++ src/edit.js
@@ -3,12 +3,70 @@
  */
 (function ($, me){
 
+    var gcanvas;
+    var gimage;
+    var gwidth;
+    var gheight;
+    var gcropper;
+    var cropCanvas;
+    var rotateAngle = 0;
     me.on('cropstart',function (evt) {
-
+        gcanvas.cropper("enable");
     });
-    /**
-     * Do preliminary setup here of dom and of script
-     * Create dropzone
-     */
+    me.on('cropcancel',function (evt) {
+        gcanvas.cropper("clear");
+        gcanvas.cropper("disable");
+    });
+    me.on('cropend', function (evt) {
+        gcanvas.cropper('replace',cropCanvas.toDataURL());
+        gcanvas.cropper('setCropData',false);
+    });
+    me.on('rotateccw', function (evt) {
+        rotateAngle -= 45;
+        gcanvas.cropper('enable');
+        gcanvas.cropper('rotate','-45');
+    });
+    me.on('rotatecw', function (evt) {
+        rotateAngle += 45;
+        gcanvas.cropper('enable');
+        gcanvas.cropper('rotate','45');
+    });
+    me.on('imageloaded',function (target) {
+            var $canvas = $(target.target);
+            var image = target.source;
+        gcanvas = $canvas;
+        gimage = image;
+                var width = $canvas.width();
+                var height = $canvas.height();
+                gwidth = width;
+                gheight = height;
+                var canvas = $canvas[0];
+                var cropper;
 
-}(jQuery,Piccolo));
+                canvas.width =  image.naturalWidth;
+                canvas.height = image.naturalHeight;
+                //console.log(canvas);
+                var factor = canvas.height/image.naturalHeight;
+                canvas.getContext('2d').drawImage(
+                    image,
+                    0, 0, image.naturalWidth , image.naturalHeight,
+                    0, 0, image.naturalWidth , image.naturalHeight
+                );
+
+            gcropper =   $canvas.cropper({
+                    aspectRatio : 1,
+                    autoCrop: false,
+                    rotatable: true,
+                    cropend:function (e) {
+                        cropCanvas = $canvas.cropper('getCroppedCanvas');
+                    },
+                    built: function () {
+                        $canvas.cropper("disable");
+                    }
+                        });
+    });
+    me.on('rotatestart',function (e) {
+
+        }
+    );
+   }(jQuery,Piccolo));
