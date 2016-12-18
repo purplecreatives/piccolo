94d46c587e8f67523fb2a5c8a5197b6675d411e9
Anos Mhazo <mhazo@zedc.co.zw>
2016-12-18 13:35:47 +0200

    Decode base64 string into an image

--- Piccolo.php
+++ Piccolo.php
@@ -20,10 +20,25 @@ class Piccolo
         $data = json_decode($payload);
 
         $this->base64_image = $data->$postvariablename;
+        $index = strpos($this->base64_image, ',') + 1;
+        $this->base64_image = substr($this->base64_image, $index);
 
     }
 
     public function save($filename){
+
+        $im = imagecreatefromstring($this->base64_image);
+
+        if($im !== false){
+
+            imagedestroy($im);
+            return true;
+
+        }else{
+
+            return false;
+
+        }
 
     }
 
