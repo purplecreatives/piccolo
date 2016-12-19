6e281d4f5e2ee9a6f01bbcdf0a05472a9a8de8db
Anos Mhazo <mhazo@zedc.co.zw>
2016-12-19 11:11:16 +0200

    Generate unique image file name

--- uploads/index.php
+++ uploads/index.php
@@ -9,6 +9,6 @@ require '../Piccolo.php';
 require '../Response.php';
 
 $piccolo = Piccolo::init('/xampp/htdocs/piccolo/uploads');
-$status = $piccolo->save('sample');
+$status = $piccolo->save(uniqid());
 
 echo json_encode(new Response($status, $piccolo));
